99443df43b073723de15c5948081adf9a0dea788
Nurhat Baydağ <baydagnurhat31@gmail.com>
2024-11-26 23:52:09 +0300

    dvsdv

--- apartmanotomasyonuFXML/src/main/java/com/mycompany/apartmanotomasyonufxml/PrimaryController.java
+++ apartmanotomasyonuFXML/src/main/java/com/mycompany/apartmanotomasyonufxml/PrimaryController.java
@@ -3,6 +3,8 @@ package com.mycompany.apartmanotomasyonufxml;
 import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -14,15 +16,155 @@ import javafx.scene.control.TextField;
 public class PrimaryController {
     
     public TextField yoneticibinano_jtf1;
+    public TextField yonetici_kayit_binano_jtf1;
+    public TextField yonetici_daire_sayisi_jtf1;
     public TextField kullanıcıbinano_jtf1;
     public TextField kullanıcıbinanokayıt_jtf1;
+    public TextField kullanıci_e_posta_jtf1;
+    public TextField kullanıcı_telefon_nokayıt_jtf1;
+    public TextField yonetici_e_posta_jtf1;
+    public TextField yoneticici_telefon_nokayıt_jtf1;
     public Label yön_gr_hata_lbli;
+     public Label kul_gr_hata_lbli;
+     public Label kul_ky_hata_lbli;
+     public Label yon_ky_hata_lbli;
     public PasswordField yöneticigirispass;
+    public PasswordField kullanicigirispass;
+    public PasswordField kullanici_kaydol_pass;
+    public PasswordField kullanici_kaydol_pass_tek;
+    public PasswordField yonetici_kaydol_pass;
+    public PasswordField yonetici_kaydol_pass_tek;
     public ComboBox dairenokayıt_cmb;
     public ComboBox dairenogiris_cmb;
     
     public static String bina_no;
     public static String daire_no;
+    @FXML
+    private void yön_kaydol() {                                             
+        SQLHelper dbhelper = new SQLHelper();
+
+        // Parametreli sorgu (önce kayıt var mı kontrol et)
+        String checkSQL = "SELECT COUNT(*) FROM yötici_kayitlari_table WHERE Bina_No = ? and Daire_Sayısı = ?";
+
+        try (ResultSet rs = dbhelper.executeQuery(checkSQL, yonetici_kayit_binano_jtf1.getText(), yonetici_daire_sayisi_jtf1.getText())) {
+            while (rs != null && rs.next()) {
+                int count = rs.getInt(1);
+                if (count > 0) {
+                    // Eğer kayıt varsa, hata mesajı
+                    yon_ky_hata_lbli.setText("Bu bina no ve daire no için zaten kayıt bulunuyor!");
+                    return;  // İşlem sonlandırılır
+                }
+            }
+        } catch (SQLException e) {
+            System.err.println("Veri çekme hatası: " + e.getMessage());
+        }
+
+        // Kayıt var mı kontrol et
+        // Eğer kayıt yoksa, yeni veriyi ekle
+        String insertSQL = "INSERT INTO yötici_kayitlari_table (Bina_No, Daire_Sayısı,şifre,Telefon_No,e_posta) VALUES (?, ?, ?,?,?)";
+        if (yonetici_kaydol_pass.getText().equals(yonetici_kaydol_pass_tek.getText())) {
+
+            // Veritabanına ekleme işlemi
+            int result = dbhelper.executeUpdate(insertSQL, yonetici_kayit_binano_jtf1.getText(), yonetici_daire_sayisi_jtf1.getText(), yonetici_kaydol_pass_tek.getText(),yoneticici_telefon_nokayıt_jtf1.getText(),yonetici_e_posta_jtf1.getText());
+            if (result > 0) {
+                System.out.println("Veri başarıyla eklendi.");
+            } else {
+                yon_ky_hata_lbli.setText("Veri ekleme başarısız.");
+                System.err.println("Veri ekleme başarısız.");
+            }
+            dbhelper.close();
+
+            if (result > 0) {
+                System.out.println("Kayıt başarıyla eklendi!");
+                yon_ky_hata_lbli.setText("Kayıt başarıyla eklendi!");
+            }
+        } else {
+            yon_ky_hata_lbli.setText("Şifre ve şifre tekrarı aynı değil.");
+        }
+
+
+    }         
+    @FXML
+    private void kullanicikaydol(){
+        SQLHelper sql = new SQLHelper();
+
+        // Parametreli sorgu (önce kayıt var mı kontrol et)
+        String checkSQL = "SELECT COUNT(*) FROM kullaniciler_table WHERE bina_no = ? AND daire_no = ?";
+        String secilenVeri = (String) dairenokayıt_cmb.getValue();
+        int index = secilenVeri.indexOf(": ");
+        String daireNoStr = secilenVeri.substring(index + 2); // ": " karakterinden sonrası
+        try (ResultSet rs = sql.executeQuery(checkSQL, kullanıcıbinanokayıt_jtf1.getText(), daireNoStr)) {
+            while (rs != null && rs.next()) {
+                int count = rs.getInt(1);
+                if (count > 0) {
+                    // Eğer kayıt varsa, hata mesajı
+                    kul_ky_hata_lbli.setText("Bu bina no ve daire no için zaten kayıt bulunuyor!");
+                    return;  // İşlem sonlandırılır
+                }
+            }
+        } catch (SQLException e) {
+            System.err.println("Veri çekme hatası: " + e.getMessage());
+        }
+
+        // Kayıt var mı kontrol et
+        // Eğer kayıt yoksa, yeni veriyi ekle
+        String insertSQL = "INSERT INTO kullaniciler_table (bina_no, daire_no, şifre,Telefon_No,e_posta) VALUES (?, ?, ?,?,?)";
+        if (kullanici_kaydol_pass.getText().equals(kullanici_kaydol_pass_tek.getText())) {
+
+            // Veritabanına ekleme işlemi
+            int result = sql.executeUpdate(insertSQL, kullanıcıbinanokayıt_jtf1.getText(), daireNoStr, kullanici_kaydol_pass_tek.getText(),kullanıcı_telefon_nokayıt_jtf1.getText(),kullanıci_e_posta_jtf1.getText());
+            if (result > 0) {
+                System.out.println("Veri başarıyla eklendi.");
+            } else {
+                System.err.println("Veri ekleme başarısız.");
+            }
+            sql.close();
+
+            if (result > 0) {
+                System.out.println("Kayıt başarıyla eklendi!");
+                kul_ky_hata_lbli.setText("Kayıt başarıyla eklendi!");
+            }
+        } else {
+            kul_ky_hata_lbli.setText("Şifre ve şifre tekrarı aynı değil.");
+        }
+    }
+    @FXML
+    private void kullanicigiris(){
+        
+        SQLHelper dbhelper = new SQLHelper();
+
+        String sql = "SELECT Bina_No, şifre FROM kullaniciler_table WHERE Bina_No = ? and daire_no = ?";
+        String girilenKullaniciAdi = kullanıcıbinano_jtf1.getText(); // Kullanıcı adını UI'den çekiyoruz
+        String secilenVeri = (String) dairenogiris_cmb.getValue();
+        int index = secilenVeri.indexOf(": ");
+        String daireNoStr = secilenVeri.substring(index + 2);
+        String girilenSifre = kullanicigirispass.getText();
+
+        try (ResultSet rs = dbhelper.executeQuery(sql, girilenKullaniciAdi, daireNoStr)) {
+
+            if (rs.next()) {
+                String veritabanindakiSifre = rs.getString("şifre");
+
+                // Girilen şifreyle veritabanındaki şifreyi karşılaştırıyoruz
+                if (veritabanindakiSifre.equals(girilenSifre)) {
+                    System.out.println("Bina_No adı ve şifre doğru, işlem başarılı!");
+                    
+                    bina_no=girilenKullaniciAdi;
+                    daire_no=daireNoStr;
+                    App.setRoot("kullaniciekrani");
+                    // Burada gerekli işlemleri yapabilirsiniz
+                } else {
+                    kul_gr_hata_lbli.setText("Bina no ve/veya şifre yanlış");
+                }
+            } else {
+                kul_gr_hata_lbli.setText("Bina no ve/veya şifre yanlış");
+            }
+        } catch (SQLException e) {
+            System.err.println("Veri çekme hatası: " + e.getMessage());
+        } catch (IOException ex) {
+            System.err.println("sayfa çekme hatası: " + ex.getMessage());
+        }
+    }
     @FXML
     private void cmbdolurkayıt() {
         // SQL bağlantı bilgileri
--- apartmanotomasyonuFXML/src/main/java/com/mycompany/apartmanotomasyonufxml/SecondaryController.java
+++ apartmanotomasyonuFXML/src/main/java/com/mycompany/apartmanotomasyonufxml/SecondaryController.java
@@ -1,12 +1,275 @@
 package com.mycompany.apartmanotomasyonufxml;
 
 import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Vector;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+
 
 public class SecondaryController {
+    @FXML
+    private CheckBox jCheckBox1;
+    @FXML
+    private CheckBox jCheckBox2;
+    @FXML
+    private CheckBox jCheckBox3;
+    @FXML
+    private CheckBox jCheckBox4;
+    @FXML
+    private CheckBox jCheckBox5;
+    @FXML
+    private CheckBox jCheckBox6;
+    @FXML
+    private TextField diger_gider_tf;
+    @FXML
+    private Label butce1;
+    @FXML
+    private Label gelir_on_hata_msj;
+    @FXML
+    private Label butce2;
+    @FXML
+    private Label aidat1;
+    @FXML
+    private Label aidat2;
+    @FXML
+    private DatePicker gelirtarih;
+    @FXML
+    private Label aidat3;
+    @FXML
+    private Spinner<Intager> aidatmiktari;
+    @FXML
+    private ComboBox daire_nocmb;
+    @FXML
+    private TableView<AidatGeliri> Gelir_table;
+    private int aidat;
+    @FXML
+    private TableColumn<AidatGeliri, Integer> binaNoColumn;
+    @FXML
+    private TableColumn<AidatGeliri, String> daireNoColumn;
+    @FXML
+    private TableColumn<AidatGeliri, String> tarihColumn;
+    @FXML
+    private TableColumn<AidatGeliri, Double> miktarColumn;
 
     @FXML
-    private void switchToPrimary() throws IOException {
-        App.setRoot("primary");
+    public void initialize(){
+         
+         binaNoColumn.setCellValueFactory(new PropertyValueFactory<>("binaNo"));
+        daireNoColumn.setCellValueFactory(new PropertyValueFactory<>("daireNo"));
+        tarihColumn.setCellValueFactory(new PropertyValueFactory<>("tarih"));
+        miktarColumn.setCellValueFactory(new PropertyValueFactory<>("miktar"));
+        butceyaz();
+        cmbdolur();
+        aidatyaz();
+        gelirlerdoldur();
+    }
+    private void cmbdolur() {
+
+        // SQL bağlantı bilgileri
+        SQLHelper dbhelper = new SQLHelper();
+
+        String sql = "SELECT Daire_Sayısı FROM yötici_kayitlari_table WHERE Bina_No = ?";
+        daire_nocmb.getItems().clear();
+        try (ResultSet rs = dbhelper.executeQuery(sql, PrimaryController.bina_no)) {
+
+            if (rs.next()) {
+                int daireSayisi = rs.getInt("Daire_Sayısı");
+                System.err.println(daireSayisi);
+
+                // Şimdi daire numaralarını ComboBox'a ekleyelim
+                ObservableList<String> daireListesi = FXCollections.observableArrayList();
+                for (int i = 1; i <= daireSayisi; i++) {
+                    daireListesi.add("Daire No: " + i);
+                }
+                daire_nocmb.setItems(daireListesi);
+            } else {
+                daire_nocmb.getItems().clear();
+            }
+        } catch (SQLException e) {
+            System.err.println("Veri çekme hatası: " + e.getMessage());
+        }
+
+    }
+    @FXML
+    private void aidat_geliri(){
+        SQLHelper dbhelper = new SQLHelper();
+        String insertSQL = "INSERT INTO aidat_gelirleri_table (bina_No, Daire_No,Tarih,miktar) VALUES (?, ?, ?,?)";
+        String secilenVeri = (String) daire_nocmb.getValue();
+        int index = secilenVeri.indexOf(": ");
+        String daireNoStr = secilenVeri.substring(index + 2);
+
+        // Veritabanına ekleme işlemi
+        int result = dbhelper.executeUpdate(insertSQL, PrimaryController.bina_no, daireNoStr, gelirtarih.getValue(), aidatmiktari.getValue());
+        if (result > 0) {
+            System.out.println("Veri başarıyla eklendi." + result);
+        } else {
+            gelir_on_hata_msj.setText("Veri ekleme başarısız.");
+            System.err.println("Veri ekleme başarısız.");
+        }
+        dbhelper.close();
+
+        if (result > 0) {
+            System.out.println("Kayıt başarıyla eklendi!");
+            gelir_on_hata_msj.setText("Kayıt başarıyla eklendi!");
+        }
+        gelirlerdoldur();
+        butceyaz();
+    }
+    public void gelirlerdoldur() {
+    SQLHelper dbhelper = new SQLHelper();
+        String sql = "SELECT bina_no, Daire_no, Tarih, miktar FROM aidat_gelirleri_table WHERE Bina_no=?";
+        ObservableList<AidatGeliri> gelirListesi = FXCollections.observableArrayList();
+
+        try (ResultSet rs = dbhelper.executeQuery(sql, PrimaryController.bina_no)) {
+            while (rs.next()) {
+                int binaNo = rs.getInt("bina_no");
+                String daireNo = rs.getString("Daire_no");
+                String tarih = rs.getString("Tarih");
+                double miktar = rs.getDouble("miktar");
+
+                // Listeye yeni satır ekle
+                gelirListesi.add(new AidatGeliri(binaNo, daireNo, tarih, miktar));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        // Tabloya verileri ekle
+        Gelir_table.setItems(gelirListesi);
+    }
+    @FXML
+    private void aidatyaz() {
+        SQLHelper dbhelper = new SQLHelper();
+
+        String sql = "SELECT aidat FROM yötici_kayitlari_table WHERE Bina_No = ?";
+        
+        try (ResultSet rs = dbhelper.executeQuery(sql, PrimaryController.bina_no)) {
+
+            if (rs.next()) {
+                aidat = rs.getInt("aidat");
+                aidat1.setText("Aidat: " + aidat);
+                aidat2.setText("Aidat: " + aidat);
+                aidat3.setText("Şimdiki Aidat: " + aidat);
+            }
+        } catch (SQLException e) {
+            System.err.println("Veri çekme hatası: " + e.getMessage());
+        }
+    }
+    @FXML
+    private void butceyaz() {
+        SQLHelper dbhelper = new SQLHelper();
+
+        String sql = "SELECT sum(miktar) toplam FROM aidat_gelirleri_table WHERE Bina_No = ?";
+
+        int toplam = 0;
+        try (ResultSet rs = dbhelper.executeQuery(sql, PrimaryController.bina_no)) {
+
+            if (rs.next()) {
+                toplam = rs.getInt("toplam");
+
+            }
+        } catch (SQLException e) {
+            System.err.println("Veri çekme hatası: " + e.getMessage());
+        }
+        String sql2 = "SELECT sum(miktar) toplam FROM Bina_Giderleri_table WHERE Bina_No = ?";
+
+        int toplam2;
+        try (ResultSet rs = dbhelper.executeQuery(sql2, PrimaryController.bina_no)) {
+
+            if (rs.next()) {
+                toplam2 = rs.getInt("toplam");
+
+                butce1.setText("Bütçe: " + (toplam - toplam2));
+                butce2.setText("Bütçe: " + (toplam - toplam2));
+            }
+        } catch (SQLException e) {
+            System.err.println("Veri çekme hatası: " + e.getMessage());
+        }
+
+    }
+    @FXML
+    private void checkbox1() throws IOException {
+        if (jCheckBox1.isSelected()) {
+            jCheckBox2.setSelected(false);
+            jCheckBox3.setSelected(false);
+            jCheckBox4.setSelected(false);
+            jCheckBox5.setSelected(false);
+            jCheckBox6.setSelected(false);
+            
+            diger_gider_tf.setDisable(true);
+        }
+    }
+    @FXML
+    private void checkbox2() throws IOException {
+        if (jCheckBox2.isSelected()) {
+            jCheckBox1.setSelected(false);
+            jCheckBox3.setSelected(false);
+            jCheckBox4.setSelected(false);
+            jCheckBox5.setSelected(false);
+            jCheckBox6.setSelected(false);
+            
+            diger_gider_tf.setDisable(true);
+        }
+    }
+    @FXML
+    private void checkbox3() throws IOException {
+        if (jCheckBox3.isSelected()) {
+            jCheckBox2.setSelected(false);
+            jCheckBox1.setSelected(false);
+            jCheckBox4.setSelected(false);
+            jCheckBox5.setSelected(false);
+            jCheckBox6.setSelected(false);
+            
+            diger_gider_tf.setDisable(true);
+        }
+    }
+    @FXML
+    private void checkbox4() throws IOException {
+        if (jCheckBox4.isSelected()) {
+            jCheckBox2.setSelected(false);
+            jCheckBox3.setSelected(false);
+            jCheckBox1.setSelected(false);
+            jCheckBox5.setSelected(false);
+            jCheckBox6.setSelected(false);
+            
+            diger_gider_tf.setDisable(true);
+        }
+    }
+    @FXML
+    private void checkbox5() throws IOException {
+        if (jCheckBox5.isSelected()) {
+            jCheckBox2.setSelected(false);
+            jCheckBox3.setSelected(false);
+            jCheckBox4.setSelected(false);
+            jCheckBox1.setSelected(false);
+            jCheckBox6.setSelected(false);
+            
+            diger_gider_tf.setDisable(true);
+        }
+    }
+    @FXML
+    private void checkbox6() throws IOException {
+        if (jCheckBox6.isSelected()) {
+            jCheckBox2.setSelected(false);
+            jCheckBox3.setSelected(false);
+            jCheckBox4.setSelected(false);
+            jCheckBox5.setSelected(false);
+            jCheckBox1.setSelected(false);
+            
+            diger_gider_tf.setDisable(false);
+        }
     }
 }
